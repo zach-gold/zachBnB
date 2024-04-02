ace6e6eff8fe3eef84deea105dd1e9e7c44bf884
Zach Gold <zackusgoldus@gmail.com>
2024-04-02 19:21:34 -0400

    made progress on spots routes

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,7 +2,72 @@ const express = require("express");
 const { Op, sequelize } = require("sequelize");
 const router = express.Router();
 const { requireAuth } = require("../../utils/auth");
-const { Spot, Review, SpotImage } = require("../../db/models");
+const { Spot, Review, SpotImage, User } = require("../../db/models");
+
+
+
+
+router.get("/current", requireAuth, async (req, res) => {
+
+  const userId = req.user.id;
+
+  const spots = await Spot.findAll({
+    where: {
+      ownerId: userId,
+    },
+  });
+    for (let spot of spots) {
+      let sum = 0;
+      const reviews = await Review.findAll({
+        where: { spotId: spot.id },
+      });
+      for (let review of reviews) {
+        sum += review.stars;
+      }
+      let average;
+      if (reviews.length > 0) {
+        average = sum / reviews.length;
+      } else {
+        average = 0;
+      }
+      spot.avgRating = average;
+      let previewImage = await SpotImage.findOne({
+        where: { spotId: spot.id, preview: true },
+        attributes: ["url"],
+      });
+      if (previewImage) {
+        spot.previewImage = previewImage;
+      }
+      else {
+        let failsafe = {
+          url: "No Image"
+        }
+        spot.previewImage = failsafe;
+      }
+    }
+
+    const allSpots = spots.map((spot) => {
+      return {
+        id: spot.id,
+        ownerId: spot.ownerId,
+        address: spot.address,
+        city: spot.city,
+        state: spot.state,
+        country: spot.country,
+        lat: spot.lat,
+        lng: spot.lng,
+        name: spot.name,
+        description: spot.description,
+        price: spot.price,
+        createdAt: spot.createdAt,
+        updatedAt: spot.updatedAt,
+        avgRating: spot.avgRating || 0,
+        previewImage: spot.previewImage.url,
+      };
+    });
+
+  res.status(200).json({ Spots: allSpots });
+});
 
 router.get("/:spotId", async (req, res) => {
   let { spotId } = req.params;
@@ -24,42 +89,62 @@ router.get("/:spotId", async (req, res) => {
   }
 
   let average;
-  if (reviews.length > 0) {
-    average = sum / reviews.length;
+  let numReviews = reviews.length;
+  spot.numReviews = numReviews;
+  if (numReviews > 0) {
+    average = sum / numReviews;
   }
+
 
   else {
     average = 0;
   }
 
-    spot.avgRating = average;
+  spot.avgRating = average;
 
     let images = await SpotImage.findAll({
-        where: { spotId: spot.id},
-        attributes: ["url"],
+      where: { spotId: spot.id },
+      attributes: ["id", "url", "preview"]
       });
-      spot.images = images;
+  spot.images = images;
+
+
+  let owner = await User.findOne({
+    where: { id: spot.ownerId },
+    attributes: ["id","firstName", "lastName"]
+  })
+
+  spot.owner = owner;
 
   if (!spot) {
     return res.status(404).json({
       message: "Spot couldn't be found",
     });
   }
-  return res.status(200).json(spot);
-});
 
 
-router.get("/current", requireAuth, async (req, res) => {
+  const returnSpot = {
+      id: spot.id,
+      ownerId: spot.ownerId,
+      address: spot.address,
+      city: spot.city,
+      state: spot.state,
+      country: spot.country,
+      lat: spot.lat,
+      lng: spot.lng,
+      name: spot.name,
+      description: spot.description,
+      price: spot.price,
+      createdAt: spot.createdAt,
+      updatedAt: spot.updatedAt,
+      numReviews: spot.numReviews,
+      avgStarRating: spot.avgRating || 0,
+      SpotImages: spot.images,
+      Owner: spot.owner,
 
-  const userId = req.user.id;
-
-  const spots = await Spot.findAll({
-    where: {
-      ownerId: userId,
-    },
-  });
-
-  res.status(200).json({ Spots: spots });
+  }
+  // console.log(spot)
+  return res.status(200).json(returnSpot);
 });
 
 router.get("/", async (req, res) => {
@@ -84,7 +169,16 @@ router.get("/", async (req, res) => {
       where: { spotId: spot.id, preview: true },
       attributes: ["url"],
     });
-    spot.previewImage = previewImage;
+
+    if (previewImage) {
+      spot.previewImage = previewImage;
+    }
+    else {
+      let failsafe = {
+        url: "No Image"
+      }
+      spot.previewImage = failsafe;
+    }
   }
 
 
