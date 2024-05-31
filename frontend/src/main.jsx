b071f5eeac5388a3bc682f3522fbe31d549c6f2b
Zach Gold <zackusgoldus@gmail.com>
2024-05-31 17:58:00 -0400

    lets go 2.0

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
 import { ModalProvider } from './context/Modal';
-//import { Modal } from './context/Modal';
+import { Modal } from './context/Modal';
 
 const store = configureStore();
 
@@ -24,6 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ModalProvider>
       <Provider store={store}>
         <App />
+        <Modal />
       </Provider>
     </ModalProvider>
   </React.StrictMode>
